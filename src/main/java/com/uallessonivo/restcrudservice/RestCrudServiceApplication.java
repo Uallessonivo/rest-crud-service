cdffd934e6dad26cbc979baa4b45e12aa99ba23a
Uallessonivo <uallessons@gmail.com>
2023-10-31 15:42:03 -0300

    first commit

---
+++ src/main/java/com/uallessonivo/restcrudservice/RestCrudServiceApplication.java
@@ -0,0 +1,13 @@
+package com.uallessonivo.restcrudservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCrudServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCrudServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uallessonivo/restcrudservice/RestCrudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uallessonivo.restcrudservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCrudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
