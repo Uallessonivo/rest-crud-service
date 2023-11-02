c3195b46855c955794df0d92821550d3945f16db
Uallesson Nunes Ivo <uallessons@gmail.com>
2023-11-02 05:55:36 -0300

    updating

--- src/main/java/com/uallessonivo/restcrudservice/controllers/StudentController.java
+++ src/main/java/com/uallessonivo/restcrudservice/controllers/StudentController.java
@@ -1,6 +1,7 @@
 package com.uallessonivo.restcrudservice.controllers;
 
 import com.uallessonivo.restcrudservice.entities.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +12,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentController {
+    private List<Student> theStudents;
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> theStudents = new ArrayList<>();
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("Mario", "Rossi"));
         theStudents.add(new Student("Luigi", "Verdi"));
         theStudents.add(new Student("Giovanni", "Bianchi"));
+    }
 
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return theStudents;
     }
 }
